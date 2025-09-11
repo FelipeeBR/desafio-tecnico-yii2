2a8a413f54f51a788792f1f708b348a2ee6c18e9
Felipe Mendes Neves <felipemen74@gmail.com>
2025-09-10 21:30:42 -0300

    feat: habilita cors

--- config/web.php
+++ config/web.php
@@ -75,6 +75,17 @@ $config = [
             'class' => 'app\modules\api\Module',
         ],
     ],
+    'as corsFilter' => [
+        'class' => \yii\filters\Cors::class,
+        'cors' => [
+            'Origin' => ['http://localhost:8080'],
+            'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+            'Access-Control-Request-Headers' => ['*'],
+            'Access-Control-Allow-Credentials' => true,
+            'Access-Control-Max-Age' => 86400,
+            'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page', 'X-Pagination-Page-Count'],
+        ],
+    ],
 ];
 
 if (YII_ENV_DEV) {
